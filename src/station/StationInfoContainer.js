c88981fecb495673a18f8b073f791eb000ebbff0
Abhishek Kc <kcabhish@gmail.com>
2017-06-18 15:40:26 -0400

    responsive initial phase

--- src/station/StationInfoContainer.js
+++ src/station/StationInfoContainer.js
@@ -7,12 +7,18 @@ import StationInfo from './StationInfo.js';
 function StationInfoContainer(props){
     
     return (
-        <div>
+        <div className="station flex-col">
+           <div className="station-header">
+              {props.stationInfo.name}
+           </div>
            <div className="station-container flex-row">
                 <StationInfo number = {props.stationInfo.free_bikes}
                              description = "free bikes"/>
                 <StationInfo number = {props.stationInfo.empty_slots}
                              description = "empty slots"/>
+           </div>
+           <div className="station-footer">
+              {props.stationInfo.timestamp}
            </div>
         </div>);
 }
